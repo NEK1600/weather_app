a2c6a5f5807e23cde1c4efb32cfde249b7971d9c
NEK1600 <nikitajw0000@gmail.com>
2023-12-09 11:26:45 +1000

    Initial commit

---
+++ lib/di/initial_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InitialApp extends StatelessWidget {
+  const InitialApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
