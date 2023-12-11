a441cd6d3fd37f09706a260c061e0891552b4f3b
NEK1600 <nikitajw0000@gmail.com>
2023-12-11 23:16:42 +1000

    сделал навигацию

---
+++ lib/presentation/navigation/navigation.dart
@@ -0,0 +1,30 @@
+import 'package:go_router/go_router.dart';
+
+abstract interface class Navigation {
+  navigate(String page);
+  showProgressDialog();
+  showBaseDialog(String message);
+}
+
+class NavigationBase implements Navigation {
+  final GoRouter router;
+  NavigationBase({required this.router});
+
+  @override
+  navigate(String page) {
+    router.goNamed(page);
+  }
+
+  @override
+  showBaseDialog(String message) {
+    Map<String, String> pathParameters = {
+      'message': message
+    };
+    router.pushNamed("dialog", extra:pathParameters);
+  }
+
+  @override
+  showProgressDialog() {
+    router.pushNamed("progressDialog");
+  }
+}
